6a50c30d2fe1971b6c40d558fc25315735894d08
Simon Guest <simon.guest@tesujimath.org>
2019-01-22 09:55:54 +1300

    Add hook for julia-repl-mode.

--- lisp/init-tesujimath-julia.el
+++ lisp/init-tesujimath-julia.el
@@ -8,5 +8,7 @@
 (require-package 'julia-repl)
 (add-to-list 'auto-mode-alist '("\\.jl\\'" . julia-mode))
 
+(add-hook 'julia-mode-hook 'julia-repl-mode)
+
 (provide 'init-tesujimath-julia)
 ;;; init-tesujimath-julia.el ends here
